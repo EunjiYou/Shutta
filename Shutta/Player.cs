72b65259671393ff0f6a1573b8bfcf9e4bae33ab
김태현 <cplkimth@naver.com>
2018-07-18 12:02:45 +0900

    .

---
+++ Shutta/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shutta
+{
+    public class Player
+    {
+        public int Money;
+
+        public void TakeCard(Card card)
+        {
+        }
+    }
+}
---
+++ Shutta/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shutta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dealer dealer = new Dealer();
+
+            Board board = new Board();
+
+            // 동적배열
+            List<Player> players = new List<Player>();
+            players.Add(new Player());
+            players.Add(new Player());
+
+            // 둘 중 한 명이 오링날 때까지
+            while (CanGoRound(players))
+            {
+                // 라운드 진행
+
+            }
+        }
+
+        static bool CanGoRound(List<Player> players)
+        {
+            if (players[0].Money > 0 && players[1].Money > 0)
+                return true;
+
+            return false;
+        }
+    }
+}
